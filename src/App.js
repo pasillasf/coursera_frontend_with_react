bae65ec63ffb349869f9771239e4c519ca28b601
Felipe <felipe.pasillas@gmail.com>
2025-05-09 13:03:32 -0400

    Hello World with React Practice

--- src/App.js
+++ src/App.js
@@ -33,13 +33,26 @@
 
 // export default App;
 
+// function App(props) {
+//     const currDate = new Date();
+  
+//     return (
+//       <div>
+//         <h1>Hello, world!</h1>
+//         <h2>The time now is {currDate.toLocaleTimeString()}.</h2>
+//       </div>
+//     );
+//   }
+  
+//   export default App;
+
 function App(props) {
     const currDate = new Date();
   
     return (
       <div>
         <h1>Hello, world!</h1>
-        <h2>The time now is {currDate.toLocaleTimeString()}.</h2>
+        <h2>It is {currDate.toLocaleDateString()} and the time now is {currDate.toLocaleTimeString()}.</h2>
       </div>
     );
   }
